9023e3ee5c98a115b39fb2d05af5077e2d92c651
amoseui <amoseui@gmail.com>
2015-03-06 00:25:59 +0900

    ch2 p124-125

--- OOAD/src/com/boutiquelab/ooad/ch2/DogDoorSimulator.java
+++ OOAD/src/com/boutiquelab/ooad/ch2/DogDoorSimulator.java
@@ -7,9 +7,17 @@ public class DogDoorSimulator {
 		System.out.println("Fido barks to go outside...");
 		remote.pressButton();
 		System.out.println("\nFido has gone outside...");
-
 		System.out.println("\nFido's all done...");
 		
+		try {
+			Thread.currentThread();
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {}
+		
+		System.out.println("...but he's stuck outside!");
+		System.out.println("\nFido starts barking...");
+		System.out.println("...so Gina grabs the remote control.");
+		remote.pressButton();
 		System.out.println("\nFido's back inside...");
 		
 	}
