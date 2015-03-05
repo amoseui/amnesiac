0ab17795bfe091c42b0847f14a449fa4bfe0a82e
amoseui <amoseui@gmail.com>
2015-03-05 23:33:01 +0900

    ch1 p66-71

--- OOAD/src/FindGuitarTester.java
+++ OOAD/src/FindGuitarTester.java
@@ -9,18 +9,19 @@ public class FindGuitarTester {
 		Inventory inventory = new Inventory();
 		initializeInventory(inventory);
 		
-		Guitar whatErinLikes = new Guitar("", 0, Builder.FENDER, "Stratocaster", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+		GuitarSpec whatErinLikes = new GuitarSpec(Builder.FENDER, "Stratocaster", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
 		
 		List<Guitar> matchingGuitars = inventory.search(whatErinLikes);
 		if (!matchingGuitars.isEmpty()) {
 			System.out.println("Erin, you might like these guitars:");
 			for (Iterator<Guitar> i = matchingGuitars.iterator(); i.hasNext(); ) {
 				Guitar guitar = (Guitar)i.next();
+				GuitarSpec spec = guitar.getSpec();
 				System.out.println("	We have a " + 
-						guitar.getBuilder() + " " + guitar.getModel() + " " + 
-						guitar.getType() + " guitar:\n	" + 
-						guitar.getBackWood() + " back and sides,\n	" + 
-						guitar.getTopWood() + " top.\nYou can have it for only $" +
+						spec.getBuilder() + " " + spec.getModel() + " " + 
+						spec.getType() + " guitar:\n	" + 
+						spec.getBackWood() + " back and sides,\n	" + 
+						spec.getTopWood() + " top.\nYou can have it for only $" +
 						guitar.getPrice() + "!\n	----");
 			}
 		} else {
@@ -30,8 +31,9 @@ public class FindGuitarTester {
 	
 	private static void initializeInventory(Inventory inventory) {
 		// 기타들을 재고 목록에 등
-		inventory.addGuitar("V95693", 1499.95, Builder.FENDER, "Stratocaster", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
-		inventory.addGuitar("V9512", 1549.95, Builder.FENDER, "Stratocaster", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+		GuitarSpec spec = new GuitarSpec(Builder.FENDER, "Stratocaster", Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+		inventory.addGuitar("V95693", 1499.95, spec);
+		inventory.addGuitar("V9512", 1549.95, spec);
 	}
 	
 }
