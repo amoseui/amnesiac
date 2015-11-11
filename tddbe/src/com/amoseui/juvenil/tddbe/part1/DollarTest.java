14f73b24c49570f2ba364f36377ebef108056c50
amoseui <amoseui@gmail.com>
2015-11-11 22:55:08 +0900

    [tddbe] part1/ch1 p41-52
    Signed-off-by: amoseui <amoseui@gmail.com>

---
+++ tddbe/src/com/amoseui/juvenil/tddbe/part1/DollarTest.java
@@ -0,0 +1,15 @@
+package com.amoseui.juvenil.tddbe.part1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DollarTest {
+
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+}
