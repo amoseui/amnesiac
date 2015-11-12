6134e4e01b15c95ff1b7d7de8d94b21a06e4b7a1
amoseui <amoseui@gmail.com>
2015-11-13 01:31:55 +0900

    [tddbe] part1/ch11 p109-112
    Signed-off-by: amoseui <amoseui@gmail.com>

--- tddbe/src/com/amoseui/juvenil/tddbe/part1/Dollar.java
+++ tddbe/src/com/amoseui/juvenil/tddbe/part1/Dollar.java
@@ -5,4 +5,8 @@ public class Dollar extends Money {
 	Dollar(int amount, String currency) {
 		super(amount, currency);
 	}
+
+	static Money dollar(int amount) {
+		return new Money(amount, "USD");
+	}
 }
--- tddbe/src/com/amoseui/juvenil/tddbe/part1/Franc.java
+++ tddbe/src/com/amoseui/juvenil/tddbe/part1/Franc.java
@@ -5,4 +5,8 @@ public class Franc extends Money {
 	Franc(int amount, String currency) {
 		super(amount, currency);
 	}
+
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
+	}
 }
